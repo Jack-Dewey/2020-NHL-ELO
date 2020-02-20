f106a2039496feb4c2e94b62f590e1302a04f66b
michaelee8 <michaelee8@gmail.com>
2020-02-20 13:15:49 -0800

    Addded codes

--- Final Project Main and Pathways.R
+++ Final Project Main and Pathways.R
@@ -17,6 +17,37 @@
 NHLwd <- getwd()
 
 
+#We set our wd as the Seasons folder
+setwd(paste(NHLwd, "/", "Seasons", "/", sep = ""))
+
+#Chooses which files to load
+nhl.seasons <- list.files(pattern="*.csv")
+
+#Forloop to load the NHL seasons csv files 2006-2019
+for (i in 1:length(nhl.seasons)) assign(nhl.seasons[i], 
+                                        read.csv(nhl.seasons[i],
+                                                 stringsAsFactors = F))
+
+#We identify the different teams in the league
+teams <- unique(NHL2006.csv$Home)
+
+#We need to identify and standardize the team names of teams that play under
+#different names currently.
+PhoenixH <- which(NHL2006.csv$Home == "Phoenix Coyotes")
+AnaheimH <- which(NHL2006.csv$Home == "Mighty Ducks of Anaheim")
+AtlantaH <- which(NHL2006.csv$Home == "Atlanta Thrashers")
+PhoenixV <- which(NHL2006.csv$Visitor == "Phoenix Coyotes")
+AnaheimV <- which(NHL2006.csv$Visitor == "Mighty Ducks of Anaheim")
+AtlantaV <- which(NHL2006.csv$Visitor == "Atlanta Thrashers")
+
+#We change the names in the dataframe for home and away
+NHL2006.csv$Home[PhoenixH] <- "Arizona Coyotes"
+NHL2006.csv$Home[AnaheimH] <- "Anaheim Ducks"
+NHL2006.csv$Home[AtlantaH] <- "Winnipeg Jets"
+NHL2006.csv$Visitor[PhoenixV] <- "Arizona Coyotes"
+NHL2006.csv$Visitor[AnaheimV] <- "Anaheim Ducks"
+NHL2006.csv$Visitor[AtlantaV] <- "Winnipeg Jets"
+
 
 
 #--------------------------------- 2. Directory Creation -----------------------
